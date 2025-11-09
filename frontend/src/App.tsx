fb7343c70d8fd2e79ef824bed845d303ba436113
martynas <martynaskvietelaitis@icloud.com>
2025-11-09 19:12:47 +0200

    Added sidebar to website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,27 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
+import SidebarMenu, {type MenuItem} from './_components/Sidebar'
+
+const menuItems: MenuItem[] = [
+    { title: 'Home', path: '/home' },
+    { title: 'Settings', path: '/settings' },
+];
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-      </Routes>
+      <div className="app-layout">
+          <SidebarMenu menuItems={menuItems} />
+          <main className="app-main-content">
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/auth" element={<AuthPage />} />
+            </Routes>
+          </main>
+      </div>
     </BrowserRouter>
   )
 }
