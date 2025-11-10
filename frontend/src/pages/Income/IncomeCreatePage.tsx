6e2c4ada8c0cd40571165c27548767f04e51e3af
girdauskaite <ugne.girdauskaite@ktu.edu>
2025-11-10 08:52:50 +0200

    Added inputs for income and budget

--- frontend/src/pages/Income/IncomeCreatePage.tsx
+++ frontend/src/pages/Income/IncomeCreatePage.tsx
@@ -1,8 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 function IncomeCreationPage() {
+    const navigate = useNavigate();
 
     return (
         <div>
-            <h1>Income Creation Page</h1>
+            <div>
+                <h1>Income Creation Page</h1>
+            </div>
+
+            <div>
+                <label>
+                    Total Amount:
+                    <input type="number" />
+                </label>
+            </div>
+
+            <button style={{ marginTop: '1rem' }} onClick={() => navigate(-1)}>
+                Create Income
+            </button>
         </div>
     );
 }
--- frontend/src/pages/Income/IncomeEditPage.tsx
+++ frontend/src/pages/Income/IncomeEditPage.tsx
@@ -1,10 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 function IncomeEditPage() {
+    const navigate = useNavigate();
 
     return (
         <div>
             <h1>Income Edit Page</h1>
 
-            <button>
+            <div>
+                <label>
+                    Total Amount:
+                    <input type="number" />
+                </label>
+            </div>
+
+            <button style={{ marginTop: '1rem' }} onClick={() => navigate(-1)}>
+                Create Income
+            </button>
+
+            <button onClick={() => navigate(-1)}>
                 Delete income
             </button>
         </div>
